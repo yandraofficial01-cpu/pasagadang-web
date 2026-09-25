13330ce70156808b2f813607aa4df02e441a286c
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-25 10:19:22 +0700

    Update inquiryform.js

--- components/inquiryform.js
+++ components/inquiryform.js
@@ -1,1 +1,5 @@
 
+'use client'
+export default function InquiryForm(){
+ return <form className="bg-white p-4 rounded-2xl shadow grid gap-3"><input className="border p-3 rounded-xl" placeholder="Nama"/><input className="border p-3 rounded-xl" placeholder="WA"/><textarea className="border p-3 rounded-xl" placeholder="Pesan"></textarea><button className="bg-black text-white p-3 rounded-xl font-black">Kirim Inquiry</button></form>
+}
