f3596c7070aa70567a7bbad7f40cb69c870567e5
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-25 10:18:55 +0700

    Update propertycard.js

--- components/propertycard.js
+++ components/propertycard.js
@@ -1,1 +1,3 @@
-
+export default function PropertyCard({p}){
+ return <div className="bg-white rounded-2xl p-4 shadow"><h3 className="font-black">{p?.title || 'Rumah Gadang Modern'}</h3><p className="text-sm text-gray-500">{p?.location || 'Padang'}</p><p className="font-bold mt-2">Rp {p?.price || '450jt'}</p></div>
+  }
