117fc3a078eaf42ae7f17489bb7f2ca73f0178dc
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-25 10:19:55 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,3 @@
-
+import './globals.css'
+import Navbar from '@/components/navbar'
+export default function RootLayout({children}){return <html><body className="bg-gray-50"><Navbar/>{children}</body></html>}
