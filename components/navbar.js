802129c3427665685ee2e94b886172b494b17a62
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-25 10:18:30 +0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,1 +1,5 @@
-
+'use client'
+import Link from 'next/link'
+export default function Navbar(){
+ return <nav className="bg-black text-white p-4 flex justify-between sticky top-0 z-50"><Link href="/" className="font-black">PASAGADANG</Link><div className="flex gap-3 text-sm font-bold"><Link href="/properties">Properti</Link><Link href="/calculator">Kalkulator</Link><Link href="/gudang">Gudang</Link></div></nav>
+}
